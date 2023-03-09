f568480467ac44d03125fef364553dc2745c4be2
faithmwamkinga <faithmwamkinga@gmail.com>
2023-03-09 08:52:30 +0300

    assesment commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,52 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+//word("Maybe","Cat")
+    //println(word(word1.indexof))
+    vocabulary()
+    println(vocabulary())
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+
+
+    var calculator = Calculator()
+    println(calculator.addition(45,56))
+    println(calculator.subtraction(86,23))
+    println(calculator.division(40,2))
+    println(calculator.multiplication(78,67))
+}
+//Write and invoke a function that takes in any 2 strings and returns the first
+//character of the longer string or the first character of the first string if they are
+//equal in length.
+//fun word (word1:String, word2:String):String{
+ //return word
+//}
+//Write and call a function that takes in an array of strings, joins them all into
+//one string and returns it.
+fun vocabulary():String{
+    var first =  arrayOf("dog", "cow")
+    var second = arrayOf("elephant", "lion")
+    var third = arrayOf("$first $second")
+    return vocabulary()
+
+
+
+}
+
+class Calculator(){
+    fun addition(num1:Int, num2:Int){
+        var sum = num1 + num2
+        println(sum)
+    }
+fun subtraction (num3:Int, num4:Int){
+    var difference = num3 - num4
+    println(difference)
+}
+    fun division (num5:Int, num6:Int){
+        var dividing = num5 / num6
+        println(dividing)
+
+    }
+    fun multiplication(num7:Int, num8:Int){
+        var product = num7 * num8
+        println(product)
+    }
 }
